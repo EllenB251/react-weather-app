33055a2502e03e86f86a4722944d3ea22b26b07e
EllenB251 <77533263+EllenB251@users.noreply.github.com>
2023-01-08 19:37:18 +0800

    Imported Project

---
+++ src/Weather.js
@@ -0,0 +1,78 @@
+import React from "react";
+import "./Weather.css";
+import axios from "axios";
+
+export default function Weather() {
+  let weatherData = {
+    city: "Perth",
+    temp: "23",
+    date: "Wednesday 21/12/2022",
+    humidity: "50",
+    wind: "10",
+    description: "Cloudy",
+    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+  };
+  return (
+    <div className="Weather">
+      <form id="search-form">
+        <div class="search-engine">
+          <label for="exampleSearch">Select Location</label>
+          <input
+            type="search"
+            class="search-engine"
+            aria-describedby="searchHelp"
+            placeholder="Search Here"
+          />
+          <button type="submit" label="submit">
+            Submit
+          </button>
+          <button type="button" class="btn btn-link">
+            Current Location
+          </button>
+        </div>
+      </form>
+      <br />
+      <div className="paralellogram">
+        <div className="today">
+          <h1>{weatherData.city}</h1>
+          <div className="row">
+            <h3 className="col-5">{weatherData.date}</h3>
+            <h3 className="col">
+              <strong>
+                <span className="digits"></span>
+                <span className="units"></span>
+              </strong>
+              <span className="units">{weatherData.temp}°C </span>
+            </h3>
+            <img src={weatherData.imgUrl} alt={weatherData.description} />
+            <span>
+              <h4></h4>
+            </span>
+            <h3 className="col"></h3>
+            <h4 className="col">
+              Humidity: <span></span>
+              {weatherData.humidity}%
+            </h4>
+            <h4 className="col">
+              Wind: <span></span>
+              {weatherData.wind}km/h
+            </h4>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className="five-days">
+        <h2>Five Day Forecast</h2>
+      </div>
+      <br />
+      <div></div>
+      <span className="final-link">
+        <a href="/" target="_blank">
+          Open-source code
+        </a>
+        by Ellen Brookes
+      </span>
+      <br />
+    </div>
+  );
+}
