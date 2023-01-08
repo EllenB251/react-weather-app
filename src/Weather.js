ac64f8b11fa9fb57a68f2c1fbd9e06c63acbc5a8
EllenB251 <77533263+EllenB251@users.noreply.github.com>
2023-01-08 20:37:47 +0800

    Undid prior test

--- src/Weather.js
+++ src/Weather.js
@@ -68,10 +68,7 @@ export default function Weather() {
       <br />
       <div></div>
       <span className="final-link">
-        <a
-          href="https://github.com/EllenB251/react-weather-app"
-          target="_blank"
-        >
+        <a href="/" target="_blank">
           Open-source code
         </a>
         <span> </span>
